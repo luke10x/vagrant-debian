e4cf23a6602296403c33f622237ea8ccd32d8a17
Lukas Normantas <baciuvys@gmail.com>
2016-01-24 23:39:29 +0100

    Custom fluxbox build + guest tools

---
+++ bootstrap-user.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p /home/$USER/src
+
+if [ ! -d /home/$USER/src/dotfiles ]; then
+    git clone git@github.com:lukoko/dotfiles.git /home/$USER/src/dotfiles || die_with "$LASTCMD"
+    (cd /home/$USER/src/dotfiles && sh install.sh) || die_with "$LASTCMD"
+fi
+
+if [ ! -d /home/$USER/src/vim ]; then
+    git clone git@github.com:lukoko/vim.git /home/$USER/src/vim || die_with "$LASTCMD"
+    (cd /home/$USER/src/vim && sh install.sh) || die_with "$LASTCMD"
+fi
+
+if [ ! -d /home/$USER/src/desktop-conf ]; then
+    git clone git@github.com:lukoko/desktop-conf.git /home/$USER/src/desktop-conf || die_with "$LASTCMD"
+    (cd /home/$USER/src/desktop-conf && sh install.sh) || die_with "$LASTCMD"
+fi
+
+if [ ! -x /home/$USER/src/desktop-conf/vendors/fluxbox/fluxbox ]; then
+    (cd /home/$USER/src/desktop-conf/vendors/fluxbox && ./autogen.sh && ./configure && make) || die_with "$LASTCMD"
+fi
+cp /home/$USER/src/desktop-conf/vendors/fluxbox/fluxbox /home/$USER/bin/fluxbox
--- bootstrap.sh
+++ bootstrap.sh
@@ -22,6 +22,12 @@ apt-get update && apt-get upgrade
 apt-get install -y -f
 apt-get install -y tree git-core vim irssi screen python-pip tmux
 apt-get install -y xorg iceweasel
+apt-get install -y xorg-dev
+apt-get install -y build-essential automake libtool autoconf pkg-config gettext
+
+apt-get install -y module-assistant
+m-a prepare
+apt-get install -y virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
 
 cp -nr $SRCDIR/home/user/.ssh/id_rsa /home/$USER/.ssh || die_with "$LASTCMD"
 cp -nr $SRCDIR/home/user/.ssh/id_rsa.pub /home/$USER/.ssh || die_with "$LASTCMD"
@@ -31,5 +37,7 @@ chmod 644 /home/$USER/.ssh/id_rsa.pub
 rm -f /home/$USER/.ssh/config
 sudo -u $USER ln -s $SRCDIR/home/user/.ssh/config /home/$USER/.ssh 2>&1
 echo "Auth keys updated succesfully"
+
+sudo -u $USER sh $SRCDIR/bootstrap-user.sh
 
 echo PROVISION FINISHED
